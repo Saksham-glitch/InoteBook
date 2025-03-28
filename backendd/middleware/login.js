b3e7c67e7c858dfea7d4e5b3f64158d9cd70ed0e
saksham jain <sakshamjain881@gmail.com>
2025-03-28 07:49:45 +0530

    first commit

---
+++ backendd/middleware/login.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+// Middleware to verify JWT token
+
+const fetchuser= (req, res, next) =>{
+    const token = req.header('auth-token');
+    if (!token)
+         return res.status(401).json({ msg: 'No token, authorization denied' });
+
+    try {
+        const data = jwt.verify(token, JWT_SECRET);
+        req.user = data.user;
+        next();
+    } catch (err) {
+        res.status(401).json({ msg: 'Token is not valid' });
+    }
+};
+
+
+module.exports = fetchuser
